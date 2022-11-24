32498d8e94eea9e68ecb679f37e553e707a59ee7
Gabriel Zafra <gabzafra@users.noreply.github.com>
2022-11-24 16:23:49 +0100

    movido metodo importar al controlodor del administrador

--- src/dam2/add/p12/MainP12.java
+++ src/dam2/add/p12/MainP12.java
@@ -1,8 +1,6 @@
 package dam2.add.p12;
 
-import java.io.File;
 import dam2.add.p12.services.Partida;
-import dam2.add.p12.services.XLSImporter;
 
 public class MainP12 {
 
@@ -13,6 +11,5 @@ public class MainP12 {
     game.reportGame();
     game.saveRecord();
     game.showHighScores();
-    XLSImporter.importXLS("ficheros" + File.separator + "preguntas.xls");
   }
 }
